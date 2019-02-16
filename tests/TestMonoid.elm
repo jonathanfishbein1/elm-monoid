ec4680f372843ad8dc2214e85e5903827ebf8549
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-16 13:19:29 -0500

    Add sum test

--- tests/TestMonoid.elm
+++ tests/TestMonoid.elm
@@ -9,14 +9,32 @@ import Test
 suite : Test.Test
 suite =
     Test.describe "The Monoid module"
-        [ Test.fuzz (Fuzz.list Fuzz.string) "tests that ++ equivalent to Monoid.concat for string" <|
+        [ Test.fuzz Fuzz.string "tests that ++ equivalent to Monoid.append for list" <|
+            \randomlyGeneratedString ->
+                let
+                    expected =
+                        String.append "" randomlyGeneratedString
+                in
+                randomlyGeneratedString
+                    |> Monoid.append Monoid.string ""
+                    |> Expect.equal expected
+        , Test.fuzz (Fuzz.list Fuzz.string) "tests that ++ equivalent to Monoid.concat for string" <|
             \randomlyGeneratedStringList ->
                 let
-                    expected : String
                     expected =
                         List.foldr (++) "" randomlyGeneratedStringList
                 in
                 randomlyGeneratedStringList
                     |> Monoid.concat Monoid.string
+                    |> Expect.equal expected
+        , Test.fuzz (Fuzz.list Fuzz.int) "tests List.sum equivalent to Monoid.concat Monoid.sum " <|
+            \randomlyGeneratedIntList ->
+                let
+                    expected =
+                        Monoid.Sum <| List.sum randomlyGeneratedIntList
+                in
+                randomlyGeneratedIntList
+                    |> List.map Monoid.Sum
+                    |> Monoid.concat Monoid.sum
                     |> Expect.equal expected
         ]
